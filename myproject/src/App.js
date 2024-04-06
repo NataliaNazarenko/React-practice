5b7f10aebe41a5f509daf858aea30dceedd0f1ea
Nataliia Nazarenko <ntuttci@gmail.com>
2024-04-06 23:37:10 +0300

    add Component

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Home from './modules/Home';
 import Forma from './modules/Forma';
+import { createElement } from 'react';
+import HelloWorldComponent from './modules/HelloWorldComponent';
+
 
 function App() {
+  const element = createElement('h1',
+  { className: 'greeting' },
+  'Hello')
   return (
     <div>
+      {element}
+      < HelloWorldComponent />
       < Home />
       < Forma />
     </div>
---
+++ myproject/src/modules/HelloWorldComponent.js
@@ -0,0 +1,5 @@
+const HelloWorldComponent = () => {
+    return <h1>I am a component</h1>;
+};
+
+export default HelloWorldComponent;
