04ff4818fdcebfd6d0dbb81f4037cf577a5fa857
Nataliia Nazarenko <ntuttci@gmail.com>
2024-04-13 22:17:46 +0300

    add key

---
+++ myproject/src/modules/ListToDo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ListToDo = ({name}) => {
+    
+    return (
+        <>
+        
+            <li>{name}</li>
+        
+        </>
+    );
+};
+
+export default ListToDo;
--- myproject/src/modules/RenderComponent.js
+++ myproject/src/modules/RenderComponent.js
@@ -1,31 +1,42 @@
 import React from "react";
+import ListToDo from "./ListToDo";
 
 const RenderComponent = () => {
     const firstRenderValue = false;
     const secondRenderValue = true;
     const thirdRenderValue = [];
     const fourthRenderValue = [1, 2, 3];
+    const toDo = [{id:1, name: 'Task 1'}, {id:2, name: 'Task 2'}, {id:3, name: 'Task 3'}, {id:4, name: 'Task 4'}];
 
-    if (firstRenderValue) {
-        return <h1>First Render</h1>;
-    };
+    // if (firstRenderValue) {
+    //     return <h1>First Render</h1>;
+    // };
 
-    if (thirdRenderValue) {
-        return <h1>Third Render</h1>;
-    };
+    // if (thirdRenderValue) {
+    //     return <h1>Third Render</h1>;
+    // };
 
-    if (secondRenderValue) {
-        return <h1>Second Render</h1>;
-    };
+    // if (secondRenderValue) {
+    //     return <h1>Second Render</h1>;
+    // };
 
 
     return (
         <div>
-            {/* {firstRenderValue && <h1>First Render</h1>}
+            {firstRenderValue && <h1>First Render</h1>}
             {secondRenderValue && <h1>Second Render</h1>}
             {!!thirdRenderValue.length && <h1>Third Render</h1>}
-            {thirdRenderValue.length ? <h1>Third Render</h1> : null} */}
+            {thirdRenderValue.length ? <h1>Third Render</h1> : null}
             {fourthRenderValue && <h1>Fourth Render</h1>}
+            
+            <ul>
+                {toDo.map((item) => {
+                console.log(item);
+                return (
+                <ListToDo key={item.id} name={item.name} />
+                );
+                })}
+            </ul>
         </div>
     );
 
