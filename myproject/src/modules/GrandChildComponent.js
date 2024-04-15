51bea23dd843cac61b5c9f3a0123e030e9af6174
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2024-04-15 22:37:35 +0300

    add GrandChildComponent

---
+++ myproject/src/modules/GrandChildComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GrandChildComponent = ({name}) => {
+    console.log(name)
+    return (
+        <>
+        <div>GrandChildComponent</div>
+        </>
+    )
+};
+
+export default GrandChildComponent;
--- myproject/src/modules/ListToDo.js
+++ myproject/src/modules/ListToDo.js
@@ -1,12 +1,13 @@
 import React from "react";
+import GrandChildComponent from './GrandChildComponent';
 
 const ListToDo = ({name}) => {
     
     return (
         <>
-        
+            
             <li>{name}</li>
-        
+            <GrandChildComponent newProp={name}/>
         </>
     );
 };
