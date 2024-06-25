6c4722d34ce5ad825b2f472db17cb6bba42bfb08
Nataliia Nazarenko <ntuttci@gmail.com>
2024-06-25 23:33:45 +0300

    add UncontrolledForm

---
+++ myproject/src/modules/components/UncontrolledForms/UncontrolledForm.js
@@ -0,0 +1,10 @@
+const UncontrolledForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <input type="submit" />
+        </form>
+    );
+};
+
+export default UncontrolledForm;
