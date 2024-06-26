93f313d06abb62f92d6f7b75e19d79f13cbb33f0
Nataliia Nazarenko <ntuttci@gmail.com>
2024-06-26 22:59:56 +0300

    add ControlledForms

--- myproject/src/modules/components/UncontrolledForms/UncontrolledForm.js
+++ myproject/src/modules/components/UncontrolledForms/UncontrolledForm.js
@@ -1,8 +1,33 @@
+import { useRef } from "react";
+
 const UncontrolledForm = () => {
+    const formRef = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // console.log(event.target.firstName.value);
+        // const name = event.target.firstName.value;
+        // const email = event.target.email.value;
+        // console.log({name: name, email: email});
+
+        console.log(formRef);
+        console.log(formRef.current);
+        console.log(formRef.current.firstName.value);
+
+        formRef.current.reset();
+
+        alert(`Input Value: ${event.target.firstName.value}`);
+    };
+
     return (
-        <form>
-            <input type="text" />
-            <input type="submit" />
+        <form ref={formRef} onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" name="firstName" id="firstName"/>
+           
+            <label htmlFor="email">email</label>
+            <input type="email" name="email" id="email"/>
+            
+            <button type="submit">Submit Form</button>
         </form>
     );
 };
