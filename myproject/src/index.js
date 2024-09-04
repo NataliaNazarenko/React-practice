97533e30281b94680dd044eb741032dcc66feb93
Nataliia Nazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2024-09-04 17:25:26 +0300

    test

--- myproject/src/index.js
+++ myproject/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
+// import {
+//   QueryClient,
+//   QueryClientProvider,
+// } from 'react-query'
 import {BrowserRouter} from "react-router-dom";
 import {createRoot} from "react-dom/client";
 
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
