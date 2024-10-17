b287e1467ade1c2359c09b007ecc4bd3e182b9f6
Nataliia Nazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2024-10-17 21:38:05 +0300

    add Counter in page About

--- myproject/src/modules/pages/About.js
+++ myproject/src/modules/pages/About.js
@@ -1,11 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import { counterSlice } from "../../redux/counter/counterSlice"
+import { useState } from "react";
 
 export default function About() {
+    const [counter, setCounter] = useState(0);
     const navigate = useNavigate();
+
+    const handlerIncrement = () => {
+        setCounter((prevState) => prevState + 1); 
+    };
+
+    const handlerDecrement = () => {
+        if (counter === 0) {
+            return;
+        };
+
+        setCounter((prevState) => prevState - 1); 
+    };
+
+    const handlerIncrementByAmount = (amount) => {
+        setCounter((prevState) => prevState + amount);
+    };
     
-    // Navigate to the home page when the button is clicked
     return <>
         <h2>About Component</h2>
         <button onClick={() => navigate('/')}>Go HomePage</button>
+        <p>Counter: {counter}</p>
+        <button onClick={handlerIncrement}>Increment</button>
+        <button onClick={handlerDecrement}>Decrement</button>
+        <button onClick={() => handlerIncrementByAmount(5)}>Increment by 5</button>
         </>;
 }
